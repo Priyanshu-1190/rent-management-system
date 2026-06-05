e51831914105110b44816ddc437cc33cb3f520a8
Priyanshu Kumar <97434308+Priyanshu-1190@users.noreply.github.com>
2026-06-05 17:09:32 +0530

    updated the dependencies

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['*'],
-  
+  turbopack: {
+    root: path.resolve(__dirname, ".."),
+  },
 };
 
 export default nextConfig;
